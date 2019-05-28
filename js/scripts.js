9e2605551afe6df7d5cc31dcbc658d09a391724a
melisajuma <Melisaakinyi95@gmail.com>
2019-05-28 16:17:31 +0300

    addition function is added

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+function add(firstNumber, secondNumber) {
+    return firstNumber+secondNumber;
+};
+var firstNumber=parseInt(prompt("Enter the firstNumber: "));
+undefined
+var secondNumber= parseInt(prompt("Enter the secondNumber:"));
+undefined
+alert(add (firstNumber, secondNumber));
